adc718dc15ac2681ccc28c7ee40173ac4a1370c6
mo7tr <mo7tr@gmail.com>
2023-10-13 16:48:55 +0100

    nodemon in dev script and dependancies + slash command handler improvement

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -6,6 +6,9 @@ const data = new SlashCommandBuilder()
 
 async function execute(interaction) {
   await interaction.reply("Pong");
+  // await interaction.followUp("Pong again!");
+
+  // throw new Error("manual error for test purpose");
 }
 
 module.exports = {
